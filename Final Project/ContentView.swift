73f9554f53c68b1c63250cab8579e442752ba51c
Maryam Zoweil <maryam.zoweil@gmail.com>
2023-06-12 10:37:22 -0500

    Initial Commit

---
+++ Final Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Final Project
+//
+//  Created by Maryam Zoweil on 6/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+//Hello
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Maryam Zoweil on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
