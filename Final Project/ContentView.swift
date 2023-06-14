db5fc77ab617cee493d411e44795b339b750dea8
Maryam Zoweil <maryam.zoweil@gmail.com>
2023-06-14 13:49:33 -0500

    After Background Color - Profile and Personal Info

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
+    @Binding var name: String
     @State private var lastName = ""
 
-    @State private var showPersonalInfo = true
+    var showPersonalInfo: Bool 
 
     var body: some View {
         ZStack {
+            Color(#colorLiteral(red: 0.968627451, green: 0.9607843137, blue: 0.9225329964, alpha: 1))
+            .ignoresSafeArea()
+            
             NavigationStack {
                 VStack {
                     Text("Profile")
@@ -33,15 +36,18 @@ struct ContentView: View {
                             .clipShape(Circle())
                     }
                     
-                    if showPersonalInfo {
+                    if !showPersonalInfo {
                         Text("Personal information missing")
                             .font(.system(.title2, design: .rounded))
                             .tint(Color(red: 51/255, green: 55/255, blue: 69/255))
                         Spacer ()
                         
-                        NavigationLink(destination: PersonalInfo(name:"", showPersonalInfo: true, lastName:"", phoneNumber:"", email: "", address: "")) {
+                        NavigationLink(destination: PersonalInfo(name:"", lastName:"", phoneNumber:"", email: "", address: "")
+                            //
+                        ) {
                             Text("Enter now")
                         }
+
                         .padding(.bottom, 200.0)
                         .buttonStyle(.borderedProminent)
                         .tint(Color(red: 0.996, green: 0.373, blue: 0.333))
@@ -58,7 +64,7 @@ struct ContentView: View {
                                     Image(systemName: "house.fill")
                                         .tint(Color(red: 51/255, green: 55/255, blue: 69/255))
                                 }
-                                NavigationLink(destination: ContentView()) {
+                                NavigationLink(destination: ContentView(name: $name, showPersonalInfo: showPersonalInfo)) {
                                     Image(systemName: "person.fill")
                                         .tint(Color(red: 51/255, green: 55/255, blue: 69/255))
                                 }
@@ -81,6 +87,6 @@ struct ContentView: View {
 //Hello
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(name: .constant(""), showPersonalInfo: false)
     }
 }
--- Final Project/PersonalInfo.swift
+++ Final Project/PersonalInfo.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct PersonalInfo: View {
     @State var name: String
-    @State var showPersonalInfo: Bool
+    var showPersonalInfo: Bool {
+        return name != ""
+    }
     @State var lastName: String
     @State var phoneNumber: String
     @State var email: String
@@ -17,68 +19,73 @@ struct PersonalInfo: View {
 
     
     var body: some View {
-        VStack {
-            //text
-            Text("Input your information")
-            .font(.system(.title, design: .rounded))
-            
-            //text field
-            TextField("First name", text: $name)
-            .multilineTextAlignment(.center)
-            .font(.system(.title, design: .rounded))
-            .border(Color(red: 0.2, green: 0.216, blue: 0.270), width: 1)
-            .padding()
-           
-            
-            //second text field
-            TextField("Last name", text: $lastName)
-            .multilineTextAlignment(.center)
-            .font(.system(.title, design: .rounded))
-            .border(Color(red: 0.2, green: 0.216, blue: 0.270), width: 1)
-            .padding()
-            
-            //third text field
-            TextField("Phone Number", text: $phoneNumber)
-            .multilineTextAlignment(.center)
-            .font(.system(.title, design: .rounded))
-            .border(Color(red: 0.2, green: 0.216, blue: 0.270), width: 1)
-            .padding()
-            
-            //fourth text field
-            TextField("Email", text: $email)
-            .multilineTextAlignment(.center)
-            .font(.system(.title, design: .rounded))
-            .border(Color(red: 0.2, green: 0.216, blue: 0.270), width: 1)
-            .padding()
-            
-            //fifth text field
-            TextField("Address", text: $address)
-            .multilineTextAlignment(.center)
-            .font(.system(.title, design: .rounded))
-            .border(Color(red: 0.2, green: 0.216, blue: 0.270), width: 1)
-            .padding()
-            
-            //button
-            
-            NavigationLink(destination: ContentView()) {
-//                //func updateVariable() {
-//                    showPersonalInfo = false
-                Text("Submit Button")
+        ZStack{
+            Color(#colorLiteral(red: 0.968627451, green: 0.9607843137, blue: 0.9225329964, alpha: 1))
+                                .ignoresSafeArea()
+            VStack {
+                //text
+                Text("Input your information")
+                    .font(.system(.title, design: .rounded))
+                
+                //text field
+                TextField("First name", text: $name)
+                    .multilineTextAlignment(.center)
+                    .font(.system(.title, design: .rounded))
+                    .border(Color(red: 0.2, green: 0.216, blue: 0.270), width: 1)
+                    .padding()
+                
+                
+                //second text field
+                TextField("Last name", text: $lastName)
+                    .multilineTextAlignment(.center)
+                    .font(.system(.title, design: .rounded))
+                    .border(Color(red: 0.2, green: 0.216, blue: 0.270), width: 1)
+                    .padding()
+                
+                //third text field
+                TextField("Phone Number", text: $phoneNumber)
+                    .multilineTextAlignment(.center)
+                    .font(.system(.title, design: .rounded))
+                    .border(Color(red: 0.2, green: 0.216, blue: 0.270), width: 1)
+                    .padding()
+                
+                //fourth text field
+                TextField("Email", text: $email)
+                    .multilineTextAlignment(.center)
+                    .font(.system(.title, design: .rounded))
+                    .border(Color(red: 0.2, green: 0.216, blue: 0.270), width: 1)
+                    .padding()
+                
+                //fifth text field
+                TextField("Address", text: $address)
+                    .multilineTextAlignment(.center)
+                    .font(.system(.title, design: .rounded))
+                    .border(Color(red: 0.2, green: 0.216, blue: 0.270), width: 1)
+                    .padding()
+                
+                //button
+                
+                NavigationLink(destination: ContentView(name:$name, showPersonalInfo: showPersonalInfo)
+                    .navigationBarBackButtonHidden(true)
+                ) {
+                    //                //func updateVariable() {
+                    //                    showPersonalInfo = false
+                    Text("Submit Button")
+                    
+                }
+                .font(.system(.title2, design: .rounded))
+                .buttonStyle(.borderedProminent)
+                .tint(Color(red: 199/255, green: 239/255, blue: 207/255))
+                .foregroundColor(Color(red: 0.2, green: 0.216, blue: 0.270))
                 
             }
-            .font(.system(.title2, design: .rounded))
-            .buttonStyle(.borderedProminent)
-            .tint(Color(red: 0xC7/255, green: 0xEF/255, blue: 0xCF/255))
-            .foregroundColor(Color(red: 0.2, green: 0.216, blue: 0.270))
             
         }
-        
-        
     }
 }
 
 struct PersonalInfo_Previews: PreviewProvider {
     static var previews: some View {
-        PersonalInfo(name:"", showPersonalInfo: false, lastName:"", phoneNumber:"", email: "", address: "")
+        PersonalInfo(name:"", lastName:"", phoneNumber:"", email: "", address: "")
     }
 }
