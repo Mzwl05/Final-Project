7ad0f391e5295d9dc02d2d91945185423366f988
Kaley Chung <megley17@gmail.com>
2023-06-15 16:49:00 -0700

    Resume File Template

--- Final Project/Resume.swift
+++ Final Project/Resume.swift
@@ -2,15 +2,98 @@
 //  Resume.swift
 //  Final Project
 //
-//  Created by Maryam Zoweil on 6/14/23.
-//
+
 
 import SwiftUI
 
 struct Resume: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+            VStack(alignment: .leading) {
+                Text("Name")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.bottom, 8)
+               
+                Text("Phone Number, Email, Address")
+                    .font(.headline)
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 16)
+               
+                Divider()
+               
+                VStack(alignment: .leading) {
+                    Text("Experience")
+                        .font(.title)
+                        .padding(.bottom, 8)
+                   
+                    Text("Company")
+                        .font(.headline)
+                        .bold()
+                        .padding(.bottom, 4)
+                   
+                    Text("Job Title")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                        .padding(.bottom, 8)
+                   
+                    Text("• Developed and maintained mobile applications using Swift and SwiftUI.")
+                    Text("• Collaborated with cross-functional teams to deliver high-quality software.")
+                    Text("• Led code reviews and provided technical guidance to junior developers.")
+                }
+                .padding(.vertical, 16)
+               
+                Divider()
+               
+                VStack(alignment: .leading) {
+                    Text("Education")
+                        .font(.title)
+                        .padding(.bottom, 8)
+                   
+                    Text("University XYZ")
+                        .font(.headline)
+                        .bold()
+                        .padding(.bottom, 4)
+                   
+                    Text("Bachelor of Science in Computer Science")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                        .padding(.bottom, 8)
+                   
+                    Text("• Relevant coursework: Algorithms, Data Structures, Software Engineering.")
+                    Text("• Graduated with honors.")
+                }
+                .padding(.vertical, 16)
+               
+                Divider()
+               
+                VStack(alignment: .leading) {
+                    Text("Honors and Awards")
+                        .font(.title)
+                        .padding(.bottom, 8)
+                   
+                    Text("Company A")
+                        .font(.headline)
+                        .bold()
+                        .padding(.bottom, 4)
+                   
+                    Text("Job Title")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                        .padding(.bottom, 8)
+                   
+                    Text("• Developed and maintained mobile applications using Swift and SwiftUI.")
+                    Text("• Collaborated with cross-functional teams to deliver high-quality software.")
+                    Text("• Led code reviews and provided technical guidance to junior developers.")
+                }
+                .padding(.vertical, 16)
+            }
+            .padding()
+       
+       
+       
+       
+        }
+
 }
 
 struct Resume_Previews: PreviewProvider {
